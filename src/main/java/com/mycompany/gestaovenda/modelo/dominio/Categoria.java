03762f7368f22f7424b12fddc2b508d611a33df0
slwill10 <wsantosdelima44@gmail.com>
2023-07-09 20:23:47 -0300

    Classe produto

--- src/main/java/com/mycompany/gestaovenda/modelo/dominio/Categoria.java
+++ src/main/java/com/mycompany/gestaovenda/modelo/dominio/Categoria.java
@@ -9,5 +9,41 @@ package com.mycompany.gestaovenda.modelo.dominio;
  * @author pc01
  */
 public class Categoria {
-    
+    private long id;
+    private String nome;
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
 }
